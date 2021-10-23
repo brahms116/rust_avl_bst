fc8b6802f693b2d4c5f5696d57a891adf1c95678
David Kwong <20544dk@gmail.com>
2021-10-23 18:24:19 +1000

    started insert

--- src/avl/mod.rs
+++ src/avl/mod.rs
@@ -2,11 +2,11 @@ pub mod node;
 
 use node::Node;
 pub struct AvlTree<K: PartialOrd, V> {
-	root: Option<Box<Node<K, V>>>,
+    root: Option<Box<Node<K, V>>>,
 }
 
 impl<K: PartialOrd, V> AvlTree<K, V> {
-	pub fn new() -> AvlTree<K, V> {
-		AvlTree { root: None }
-	}
+    pub fn new() -> AvlTree<K, V> {
+        AvlTree { root: None }
+    }
 }
